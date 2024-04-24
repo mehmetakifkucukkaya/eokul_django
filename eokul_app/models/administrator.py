5baea3adcec76c0d7aee73419e5ac59ca110818b
mehmetakifkucukkaya <mehmet.akif.20021@gmail.com>
2024-04-24 13:55:07 +0300

    Admin validation işlemleri yapıldı

--- eokul_app/models/administrator.py
+++ eokul_app/models/administrator.py
@@ -1,9 +1,15 @@
 from django.db import models
-
+from django.core.validators import EmailValidator, MinLengthValidator
 from eokul_app.models.base import BaseModel
 
-
 class Administrator(BaseModel):
+    MUDUR = 'Mudur'
+    OGRETMEN = 'Ogretmen'
+
+    AUTHORITY_CHOICES = [
+        (MUDUR, 'Müdür'),
+        (OGRETMEN, 'Öğretmen'),
+    ]
 
     class Meta(object):
         db_table = "administrator"
@@ -11,8 +17,7 @@ class Administrator(BaseModel):
 
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    username = models.CharField(max_length=16)
-    password = models.CharField(max_length=16)
-    authority = models.CharField(max_length=15)
+    username = models.CharField(max_length=16,validators=[MinLengthValidator(6)])
+    password = models.CharField(max_length=16 ,validators=[MinLengthValidator(8)])
+    authority = models.CharField(max_length=15, choices=AUTHORITY_CHOICES)
 
-  
