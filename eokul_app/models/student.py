5c85cb98b53bfae788acbe3e018950900c1d3317
mehmetakifkucukkaya <mehmet.akif.20021@gmail.com>
2024-04-24 13:40:29 +0300

    Student için validation işlemleri yapıldı

--- eokul_app/models/student.py
+++ eokul_app/models/student.py
@@ -11,6 +11,28 @@ def validate_grade(value):
             params={'value': value},
         )
 
+def validate_tc(value):
+    if (len(value) != 11 ):
+        raise ValidationError(
+            'TC No "%(value)s" is not valid. TC must be exactly 11 digits ',
+            params={'value': value},
+        )
+    
+def validate_std_no(value):
+    if (len(value) != 6 ):
+        raise ValidationError(
+            'Student No "%(value)s" is not valid. Student No must be exactly 6 digits ',
+            params={'value': value},
+        )  
+
+def validate_absentee(value):
+    if (value < 0 ):
+        raise ValidationError(
+            'Absentee "%(value)s" is not valid. Absentee must be greater than 0',
+            params={'value': value},
+        ) 
+        
+
 class Student(BaseModel):
     class Meta(object):
         db_table = "student"
@@ -18,7 +40,7 @@ class Student(BaseModel):
 
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    tc_no = models.CharField(max_length=11)
-    std_no = models.CharField(max_length=6)
+    tc_no = models.CharField(max_length=11 , validators=[validate_tc])
+    std_no = models.CharField(max_length=6 , validators=[validate_std_no])
     grade = models.CharField(max_length=2, validators=[validate_grade])
-    absentee = models.IntegerField(default=0)
+    absentee = models.IntegerField(default=0, validators=[validate_absentee])
