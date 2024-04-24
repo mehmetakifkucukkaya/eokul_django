e9955ea1dcf82e74d5c2dbec60090c63daa91e84
BİLİ <121384275+baharsevinti@users.noreply.github.com>
2024-04-24 14:54:09 +0300

    Lesson validation işlemi yapıldı

--- eokul_app/models/lesson.py
+++ eokul_app/models/lesson.py
@@ -1,7 +1,7 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 from eokul_app.models.base import BaseModel
-
 
 class Lesson(BaseModel):
 
@@ -9,7 +9,14 @@ class Lesson(BaseModel):
         db_table = "lesson"
         app_label = "eokul_app"
 
-    lesson_code = models.CharField(max_length=6)
-    lesson_text = models.CharField(max_length=50)
+    # yalnızca harfle başlamak için 
+    starts_with_letter = RegexValidator(
+        regex=r'^[a-zA-Z].*$', 
+        message="Lesson code must start with a letter."
+    )
 
-    
+    lesson_code = models.CharField(
+        max_length=6,
+        validators=[starts_with_letter]
+    )
+    lesson_text = models.CharField(max_length=50)
