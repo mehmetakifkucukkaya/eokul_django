8f9d6a8ec7b04c7c56a01b284113563909226386
mehmetakifkucukkaya <mehmet.akif.20021@gmail.com>
2024-04-25 15:56:58 +0300

    students_lesson html ve views yazıldı

--- eokul_app/views/lesson_views.py
+++ eokul_app/views/lesson_views.py
@@ -1,10 +1,22 @@
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 from django.urls import reverse
 from eokul_app.permission import is_student_login
+from eokul_app.models import Student, StudentsLesson, Lesson
 
 def list_lesson_by_std_no(req, std_no):
     if is_student_login(req, std_no):
-        return HttpResponse(std_no + " için dersler")
+        student = Student.objects.filter(std_no = std_no).first()
+        student_lessons = list(StudentsLesson.objects.filter(student = student.id))
+        lessons = []
+        for lesson in student_lessons:
+            lessons.add(Lesson.objects.filter(pk = lesson).first())
+            
+        context = {
+            "lessons" : lessons
+        }
+        
+        return render(req, "eokul_app/students_lesson.html", context)
     else:
         redirect_url = reverse("index")
         return HttpResponseRedirect(redirect_url)
