89aa0686a09afdf29b1eccc7851371b4531d592b
mehmetakifkucukkaya <mehmet.akif.20021@gmail.com>
2024-04-26 10:55:56 +0300

    Derslerin gösterilme sorunu düzeltildi

--- eokul_app/views/lesson_views.py
+++ eokul_app/views/lesson_views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from eokul_app.models.weekly_shedule import WeeklyShedule
 from eokul_app.permission import is_student_login
 from eokul_app.models import Student, StudentsLesson, Lesson
 
@@ -8,12 +9,14 @@ def list_lesson_by_std_no(req, std_no):
     if is_student_login(req, std_no):
         student = Student.objects.filter(std_no = std_no).first()
         student_lessons = list(StudentsLesson.objects.filter(student = student.id))
-        lessons = []
-        for lesson in student_lessons:
-            lessons.add(Lesson.objects.filter(pk = lesson).first())
+        lessons = Lesson.objects.all()
+        # for lesson in student_lessons:
+        #     lessons.add(Lesson.objects.filter(pk = lesson).first())
+        
+        
             
         context = {
-            "lessons" : lessons
+            "lessons" : lessons,
         }
         
         return render(req, "eokul_app/students_lesson.html", context)
